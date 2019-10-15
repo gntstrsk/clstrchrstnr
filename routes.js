e2bba13d9dbee9f9fb2737318328cfab8edfa0e6
gntstrsk <56361733+gntstrsk@users.noreply.github.com>
2019-10-16 02:53:28 +0800

    Added cluster name generation based on given json data.

--- routes.js
+++ routes.js
@@ -1,10 +1,27 @@
 // General Requirements
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 router.get('/', (req, res, next) => {
+
+  // Parse JSON File
+  const rawData = fs.readFileSync('keywords.json');
+  const parsedData = JSON.parse(rawData);
+
+  // Get Random Name per Keyword
+  const list = [];
+  let index = 0;
+  for (const keyword of parsedData.keywords) {
+    index = Math.floor(Math.random() * keyword.items.length);
+    list.push(keyword.items[index].name);
+  }
+
+  const journalName = list.join(' ');
+  
+  // Return Data
   res.status(200).json({
-    message: 'Yametekudasai!'
+    journal_name: journalName
   });
 });
 
